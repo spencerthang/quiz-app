018b1bbbef300658e487b7dd7648c77e735d4f17
Spencer Thang <spencerthang@gmail.com>
2020-09-17 19:58:08 -0400

    Add yarn built files.

---
+++ server/static/precache-manifest.cccb1c2e1886a3c4c1a293e693fadd08.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c1b5e6f567fa661bbcbdd1bc094777",
+    "url": "/static/index.html"
+  },
+  {
+    "revision": "72dea09d2e554b608cce",
+    "url": "/static/static/css/2.55f9e7e8.chunk.css"
+  },
+  {
+    "revision": "091799540f0d5f0fd7d5",
+    "url": "/static/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "72dea09d2e554b608cce",
+    "url": "/static/static/js/2.ef342c2e.chunk.js"
+  },
+  {
+    "revision": "9944a45902f915d9aae4db0c09e83634",
+    "url": "/static/static/js/2.ef342c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "091799540f0d5f0fd7d5",
+    "url": "/static/static/js/main.eacec25c.chunk.js"
+  },
+  {
+    "revision": "a7f3dd44261183e0d650",
+    "url": "/static/static/js/runtime-main.89bf0eb4.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/static/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/static/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/static/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/static/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/static/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/static/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
